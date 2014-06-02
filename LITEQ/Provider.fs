f6441b1f13fc697150820d99ea2f13d981729acd
Martin Leinberger <martin.leinberger@googlemail.com>
2014-06-02 14:36:34 +0200

    Added skeleton for type provider

--- LITEQ/Provider.fs
+++ LITEQ/Provider.fs
@@ -1,10 +1,32 @@
 ﻿namespace LITEQ
 
-open SPARQLHttpProtocol
 open NPQL
 open TypeMapper
 open SchemaInformation
+open SPARQLHttpProtocol
 
-type Class1() = 
-    member this.X = "F#"
-    member this.Abc x = mysupercoolfunction x
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Collections
+open ProviderImplementation.ProvidedTypes
+open System.Reflection
+
+[<TypeProvider>]
+type RDFTypeProvider(config : TypeProviderConfig) as this = 
+    class
+        inherit TypeProviderForNamespaces()
+        let ns = "Uniko.West"
+        let asm = Assembly.GetExecutingAssembly()
+        let provTy = ProvidedTypeDefinition(asm, ns, "RDFStore", Some typeof<obj>)
+        
+        let buildTypes (typeName : string) (myParam : string) = 
+            let t = ProvidedTypeDefinition(className = typeName, baseType = None)
+            provTy.AddMember t
+            t
+        
+        let parameters = [ ProvidedStaticParameter("myParameter", typeof<string>) ]
+        do provTy.DefineStaticParameters(parameters, fun typeName args -> buildTypes typeName (args.[0] :?> string))
+        do this.AddNamespace(ns, [ provTy ])
+    end
+
+[<TypeProviderAssembly>]
+do ()
